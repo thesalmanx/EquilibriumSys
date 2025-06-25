a53ece491e954f799fb8440ef350149108a2867f
thesalmanx <thesalmanx@gmail.com>
2025-06-25 23:41:03 +0500

    feat:changes done

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -39,7 +39,7 @@ export async function POST(request: NextRequest) {
         name,
         email,
         password: hashedPassword,
-        role: 'STAFF',
+        role: 'ADMIN',
       },
       select: {
         id: true,
